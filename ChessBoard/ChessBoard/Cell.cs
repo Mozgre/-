2f4e6f4fdda6786a6b77fab33e4ba9e40f00fb6a
Mozgre <gaz5328@gmail.com>
2023-05-30 09:48:00 +0300

    init

---
+++ ChessBoard/ChessBoard/Cell.cs
@@ -0,0 +1,27 @@
+﻿namespace ChessBoard
+{
+    public class Cell : NotifyPropertyChanged
+    {
+        private State _state;
+        private bool _active;
+
+        public State State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool Active
+        {
+            get => _active;
+            set
+            {
+                _active = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
